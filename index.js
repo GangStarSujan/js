94bdafcc4cbba3bab19d22b7509fc3e59c81ef74
GangStarSujan <153915654+GangStarSujan@users.noreply.github.com>
2024-01-01 17:20:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const names = ["sujan" , "jegovaa" , "guna"];
+var phonenumber = [996232873669];
+var userName = prompt("Please enter your name:");
+
+if (userName == names) {
+    alert("Hello, " + userName + "thanks for comming");
+} else {
+    alert("your name " + userName + "not in the list");
+}
